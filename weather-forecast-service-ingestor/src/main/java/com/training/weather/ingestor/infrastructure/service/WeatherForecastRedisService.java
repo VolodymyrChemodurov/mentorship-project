d1c2c185e9fa811bc436286f0733e7667efd2d31
OlegHudyma <id27041999.video@gmail.com>
2018-07-14 00:36:52 +0300

    [MLG-1] Changed field injection to constructor injection.

--- weather-forecast-service-ingestor/src/main/java/com/training/weather/ingestor/infrastructure/service/WeatherForecastRedisService.java
+++ weather-forecast-service-ingestor/src/main/java/com/training/weather/ingestor/infrastructure/service/WeatherForecastRedisService.java
@@ -4,9 +4,7 @@ import com.training.weather.ingestor.infrastructure.entity.City;
 import com.training.weather.ingestor.infrastructure.entity.Forecast;
 import com.training.weather.ingestor.infrastructure.entity.redis.WeatherForecast;
 import com.training.weather.ingestor.infrastructure.repository.WeatherForecastRedisRepository;
-
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.geo.Point;
 import org.springframework.stereotype.Service;
 
@@ -14,8 +12,11 @@ import org.springframework.stereotype.Service;
 public class WeatherForecastRedisService {
   private static final Logger LOG = Logger.getLogger(WeatherForecastRedisService.class);
 
-  @Autowired
-  private WeatherForecastRedisRepository weatherForecastRedisRepository;
+  private final WeatherForecastRedisRepository weatherForecastRedisRepository;
+
+  public WeatherForecastRedisService(WeatherForecastRedisRepository weatherForecastRedisRepository) {
+    this.weatherForecastRedisRepository = weatherForecastRedisRepository;
+  }
 
   /**
    * Method for storing weather forecast to Redis.
