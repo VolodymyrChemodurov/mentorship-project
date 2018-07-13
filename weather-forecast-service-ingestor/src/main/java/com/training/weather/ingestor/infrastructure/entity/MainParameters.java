775c16bc04ccb0fdae1aa1c198a41a6416905c5e
OlegHudyma <id27041999.video@gmail.com>
2018-07-13 22:38:19 +0300

    [MLG-1] Renamed "Main" to "MainParameters". Removed Lombok.

---
+++ weather-forecast-service-ingestor/src/main/java/com/training/weather/ingestor/infrastructure/entity/MainParameters.java
@@ -0,0 +1,125 @@
+
+package com.training.weather.ingestor.infrastructure.entity;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "temp",
+        "temp_min",
+        "temp_max",
+        "pressure",
+        "sea_level",
+        "grnd_level",
+        "humidity",
+        "temp_kf"})
+public class MainParameters {
+
+  @JsonProperty("temp")
+  private Double temperature;
+  @JsonProperty("temp_min")
+  private Double minTemperature;
+  @JsonProperty("temp_max")
+  private Double maxTemperature;
+  @JsonProperty("pressure")
+  private Double pressure;
+  @JsonProperty("sea_level")
+  private Double seaLevelPressure;
+  @JsonProperty("grnd_level")
+  private Double groundLevelPressure;
+  @JsonProperty("humidity")
+  private Integer humidity;
+  @JsonProperty("temp_kf")
+  private Integer tempKf;
+
+  public MainParameters() {
+  }
+
+  /**
+   * Constructor.
+   * @param temperature
+   * @param minTemperature
+   * @param maxTemperature
+   * @param pressure
+   * @param seaLevelPressure
+   * @param groundLevelPressure
+   * @param humidity
+   * @param tempKf
+   */
+  public MainParameters(Double temperature, Double minTemperature, Double maxTemperature, Double pressure, Double seaLevelPressure, Double groundLevelPressure, Integer humidity, Integer tempKf) {
+    this.temperature = temperature;
+    this.minTemperature = minTemperature;
+    this.maxTemperature = maxTemperature;
+    this.pressure = pressure;
+    this.seaLevelPressure = seaLevelPressure;
+    this.groundLevelPressure = groundLevelPressure;
+    this.humidity = humidity;
+    this.tempKf = tempKf;
+  }
+
+  public Double getTemperature() {
+    return temperature;
+  }
+
+  public void setTemperature(Double temperature) {
+    this.temperature = temperature;
+  }
+
+  public Double getMinTemperature() {
+    return minTemperature;
+  }
+
+  public void setMinTemperature(Double minTemperature) {
+    this.minTemperature = minTemperature;
+  }
+
+  public Double getMaxTemperature() {
+    return maxTemperature;
+  }
+
+  public void setMaxTemperature(Double maxTemperature) {
+    this.maxTemperature = maxTemperature;
+  }
+
+  public Double getPressure() {
+    return pressure;
+  }
+
+  public void setPressure(Double pressure) {
+    this.pressure = pressure;
+  }
+
+  public Double getSeaLevelPressure() {
+    return seaLevelPressure;
+  }
+
+  public void setSeaLevelPressure(Double seaLevelPressure) {
+    this.seaLevelPressure = seaLevelPressure;
+  }
+
+  public Double getGroundLevelPressure() {
+    return groundLevelPressure;
+  }
+
+  public void setGroundLevelPressure(Double groundLevelPressure) {
+    this.groundLevelPressure = groundLevelPressure;
+  }
+
+  public Integer getHumidity() {
+    return humidity;
+  }
+
+  public void setHumidity(Integer humidity) {
+    this.humidity = humidity;
+  }
+
+  public Integer getTempKf() {
+    return tempKf;
+  }
+
+  public void setTempKf(Integer tempKf) {
+    this.tempKf = tempKf;
+  }
+}
