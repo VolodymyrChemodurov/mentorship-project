ae2fbfa806b4c9d4e27ba6f48b6c66e6ba9066c0
OlegHudyma <id27041999.video@gmail.com>
2018-07-15 20:05:48 +0300

    [MLG-1] Added configuration for RestTemplate JSON deserializetion.

--- weather-forecast-service-ingestor/src/main/java/com/training/weather/ingestor/infrastructure/config/ApplicationConfig.java
+++ weather-forecast-service-ingestor/src/main/java/com/training/weather/ingestor/infrastructure/config/ApplicationConfig.java
@@ -1,8 +1,10 @@
 package com.training.weather.ingestor.infrastructure.config;
 
-import com.training.weather.ingestor.infrastructure.entity.City;
-import com.training.weather.ingestor.infrastructure.entity.Coordinates;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.training.weather.ingestor.core.model.*;
 import com.training.weather.ingestor.infrastructure.entity.redis.WeatherForecast;
+import com.training.weather.ingestor.infrastructure.util.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,6 +23,7 @@ import java.util.List;
 
 /**
  * Spring configuration and Bean definition.
+ *
  * @author Oleg_Hudyma
  */
 @Configuration
@@ -27,19 +31,73 @@ import java.util.List;
 @EnableScheduling
 @EnableRedisRepositories
 public class ApplicationConfig {
+
+  /**
+   * ObjectMapper for deserializing json.
+   *
+   * @return {@link ObjectMapper}
+   */
   @Bean
-  public RestTemplate restTemplate() {
-    return new RestTemplate();
+  public ObjectMapper objectMapper() {
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+    objectMapper.addMixIn(Coordinates.class, CoordinatesMixIn.class);
+    objectMapper.addMixIn(City.class, CityMixIn.class);
+    objectMapper.addMixIn(Clouds.class, CloudsMixIn.class);
+    objectMapper.addMixIn(Wind.class, WindMixIn.class);
+    objectMapper.addMixIn(Rain.class, RainMixIn.class);
+    objectMapper.addMixIn(Snow.class, SnowMixIn.class);
+    objectMapper.addMixIn(MainParameters.class, MainParametersMixIn.class);
+    objectMapper.addMixIn(Forecast.class, ForecastMixIn.class);
+    objectMapper.addMixIn(OpenWeatherMapResponse.class, OpenWeatherMapResponseMixIn.class);
+
+    return objectMapper;
   }
 
   /**
-   * Definition of available cities.
+   * Defines message  converter based on Object Mapper.
+   *
+   * @param objectMapper ObjectMapper
+   * @return {@link MappingJackson2HttpMessageConverter}
+   */
+  @Bean
+  public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter(
+          ObjectMapper objectMapper) {
+    MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+
+    converter.setObjectMapper(objectMapper);
+
+    return converter;
+  }
+
+  /**
+   * Defines Rest Template.
+   *
+   * @param mappingJackson2HttpMessageConverter MappingJackson2HttpMessageConverter
+   * @return {@link RestTemplate}
+   */
+  @Bean
+  public RestTemplate restTemplate(
+          MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter
+  ) {
+    RestTemplate restTemplate = new RestTemplate();
+    restTemplate.getMessageConverters().removeIf(m -> m.getClass().getName().equals(MappingJackson2HttpMessageConverter.class.getName()));
+    restTemplate.getMessageConverters().add(mappingJackson2HttpMessageConverter);
+
+    return restTemplate;
+  }
+
+  /**
+   * Defines list of available cities.
+   * @return {@link List&ltCity&gt}
    */
   @Bean
   public List<City> cities() {
     List<City> cities = new ArrayList<>();
-    cities.add(new City(null, "Lviv", new Coordinates(49.838261, 24.023239), "UA"));
-    cities.add(new City(null, "Mykolayiv", new Coordinates(49.52372,23.98522), "UA"));
+    cities.add(new City("Lviv", new Coordinates(49.838261, 24.023239), "UA"));
+    cities.add(new City("Mykolayiv", new Coordinates(49.52372, 23.98522), "UA"));
     return cities;
   }
 
@@ -55,8 +113,8 @@ public class ApplicationConfig {
   public RedisTemplate<String, WeatherForecast> redisTemplate(
           RedisConnectionFactory connectionFactory) {
     RedisTemplate<String, WeatherForecast> redisTemplate = new RedisTemplate<>();
-    redisTemplate.setKeySerializer( new StringRedisSerializer() );
-    redisTemplate.setHashValueSerializer( new GenericToStringSerializer<>(WeatherForecast.class ) );
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setHashValueSerializer(new GenericToStringSerializer<>(WeatherForecast.class));
     redisTemplate.setValueSerializer(new GenericToStringSerializer<>(WeatherForecast.class));
     redisTemplate.setConnectionFactory(connectionFactory);
     return redisTemplate;
