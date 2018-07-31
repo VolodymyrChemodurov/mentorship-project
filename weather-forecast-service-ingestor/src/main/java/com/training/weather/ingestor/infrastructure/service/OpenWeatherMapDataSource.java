936a7d18f11718009ebc4e9b60c9d957c52c3efb
OlegHudyma <id27041999.video@gmail.com>
2018-07-31 22:37:09 +0300

    [MLG-1]
    -Replaced hardcoded values with values from application.properties.

--- weather-forecast-service-ingestor/src/main/java/com/training/weather/ingestor/infrastructure/service/OpenWeatherMapDataSource.java
+++ weather-forecast-service-ingestor/src/main/java/com/training/weather/ingestor/infrastructure/service/OpenWeatherMapDataSource.java
@@ -1,22 +1,29 @@
 package com.training.weather.ingestor.infrastructure.service;
 
 import com.training.weather.ingestor.core.model.Coordinates;
-import com.training.weather.ingestor.core.model.OpenWeatherMapResponse;
-import org.apache.log4j.Logger;
+import com.training.weather.ingestor.core.service.WeatherDataSource;
+import com.training.weather.ingestor.infrastructure.entity.redis.OpenWeatherMapResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-
 @Service
 public class OpenWeatherMapDataSource implements WeatherDataSource {
-  private static final Logger LOG = Logger.getLogger(OpenWeatherMapDataSource.class);
+  private static final Logger LOG = LoggerFactory.getLogger(OpenWeatherMapDataSource.class);
 
-  private static final String API_SCHEME = "http";
-  private static final String API_HOST = "api.openweathermap.org";
-  private static final String API_KEY = "a5b6e7041392ba7156b0d1de0e3e7923";
+  @Value("${owm.api.scheme}")
+  private String API_SCHEME;
+
+  @Value("${owm.api.host}")
+  private String API_HOST;
+
+  @Value("${owm.api.key}")
+  private String API_KEY;
 
   private final RestTemplate restTemplate;
 
@@ -35,9 +42,13 @@ public class OpenWeatherMapDataSource implements WeatherDataSource {
                     coordinates.getLatitude(),
                     coordinates.getLongitude(), API_KEY);
 
+    LOG.info("Retrieving forecasts from OWM for: " + coordinates);
     ResponseEntity<OpenWeatherMapResponse> response = restTemplate
             .getForEntity(uriComponents.toUri(), OpenWeatherMapResponse.class);
+    OpenWeatherMapResponse openWeatherMapResponse = response.getBody();
+    openWeatherMapResponse.setCoordinates(coordinates);
 
-    return response.getBody();
+    LOG.info("Successfully retrieved forecasts " + openWeatherMapResponse);
+    return openWeatherMapResponse;
   }
 }
