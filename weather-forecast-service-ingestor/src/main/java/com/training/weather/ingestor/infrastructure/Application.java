e10edd223952e479d3317663a2587c7817e899f5
OlegHudyma <id27041999.video@gmail.com>
2018-09-02 22:30:44 +0300

    [MLG-2]
    -Added profiles based on Java args(local, dev, stress).

--- weather-forecast-service-ingestor/src/main/java/com/training/weather/ingestor/infrastructure/Application.java
+++ weather-forecast-service-ingestor/src/main/java/com/training/weather/ingestor/infrastructure/Application.java
@@ -1,12 +1,13 @@
 package com.training.weather.ingestor.infrastructure;
 
 import com.training.weather.ingestor.infrastructure.config.ApplicationConfig;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Import;
 
 /**
  * Class which starts Spring Boot application.
+ *
  * @author Oleg_Hudyma
  */
 @SpringBootApplication
@@ -14,7 +15,28 @@ import org.springframework.context.annotation.Import;
 @SuppressWarnings("PMD.UseUtilityClass")
 public class Application {
 
+  /**
+   * Builds and runs Spring Boot Application.
+   *
+   * @param args Java command line args.
+   */
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    new SpringApplicationBuilder(Application.class)
+            .properties(getPropertiesFileName(args[0]))
+            .build()
+            .run(args);
+  }
+
+  /**
+   * Used for building Spring property for configuring properties file name.
+   *
+   * @param args Java command line arguments.
+   * @return String. Spring property for configuring properties file name.
+   */
+  private static String getPropertiesFileName(String args) {
+    String propertiesFileName = "application-" + args;
+    String propertiesFileKey = "spring.config.name";
+
+    return propertiesFileKey + ":" + propertiesFileName;
   }
 }
