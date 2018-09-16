dc20d2157071e5445c6240bf525dc55df4d263e7
OlegHudyma <id27041999.video@gmail.com>
2018-09-16 23:00:40 +0300

    MLG-2
    -Using Environment variable for properties file.

--- weather-forecast-service-ingestor/src/main/java/com/training/weather/ingestor/infrastructure/Application.java
+++ weather-forecast-service-ingestor/src/main/java/com/training/weather/ingestor/infrastructure/Application.java
@@ -1,8 +1,8 @@
 package com.training.weather.ingestor.infrastructure;
 
 import com.training.weather.ingestor.infrastructure.config.ApplicationConfig;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Import;
 
 /**
@@ -21,22 +21,6 @@ public class Application {
    * @param args Java command line args.
    */
   public static void main(String[] args) {
-    new SpringApplicationBuilder(Application.class)
-            .properties(getPropertiesFileName(args[0]))
-            .build()
-            .run(args);
-  }
-
-  /**
-   * Used for building Spring property for configuring properties file name.
-   *
-   * @param args Java command line arguments.
-   * @return String. Spring property for configuring properties file name.
-   */
-  private static String getPropertiesFileName(String args) {
-    String propertiesFileName = "application-" + args;
-    String propertiesFileKey = "spring.config.name";
-
-    return propertiesFileKey + ":" + propertiesFileName;
+    SpringApplication.run(Application.class);
   }
 }
